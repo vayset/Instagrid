83e7849cf45da05ef5ec9f7a85b2054e8ae96591
Saddam <saddamwolf@gmail.com>
2020-05-17 03:16:14 +0200

    Initial Commit

---
+++ Instagrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Saddam Satouyev on 17/05/2020.
+//  Copyright © 2020 Saddam Satouyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
