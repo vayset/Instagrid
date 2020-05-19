e94e665015dd040b7bf34557ab44db91658b3a76
Saddam <saddamwolf@gmail.com>
2020-05-19 20:35:41 +0200

    Premier push projet instagrid

---
+++ Instagrid/Other/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Instagrid
+//
+//  Created by Saddam Satouyev on 17/05/2020.
+//  Copyright © 2020 Saddam Satouyev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate { }
+
---
+++ Instagrid/Other/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  Instagrid
+//
+//  Created by Saddam Satouyev on 17/05/2020.
+//  Copyright © 2020 Saddam Satouyev. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+
